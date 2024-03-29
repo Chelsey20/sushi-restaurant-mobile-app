22bdf95d8936c6bfb024e292e70065f91fb8f678
clc <68854856+Chelsey20@users.noreply.github.com>
2024-03-29 20:38:45 +0800

    add to cart ui display

--- sushi_restaurant_app/lib/pages/food_details_page.dart
+++ sushi_restaurant_app/lib/pages/food_details_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sushi_restaurant_app/themes/colors.dart';
 import '../models/food.dart';
 
 class FoodDetailsPage extends StatefulWidget {
@@ -11,6 +13,16 @@ class FoodDetailsPage extends StatefulWidget {
 }
 
 class _FoodDetailsPageState extends State<FoodDetailsPage> {
+  //quantity count
+  int quantityCount = 0;
+
+  //decrement quantity
+  void decrementQuantity() {
+    setState(() {
+      quantityCount--;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,42 +34,119 @@ class _FoodDetailsPageState extends State<FoodDetailsPage> {
         body: Column(
           children: [
             Expanded(
-              child: ListView(
-                children: [
-                  //image
-                  Image.asset(
-                    widget.food.imagePath,
-                    height: 200,
-                  ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: ListView(
+                  children: <Widget>[
+                    //image
+                    Image.asset(
+                      widget.food.imagePath,
+                      height: 200,
+                    ),
 
-                  const SizedBox(height: 25),
+                    const SizedBox(height: 25),
 
-                  Row(
-                    children: [
-                      //star icon
-                      Icon(
-                        Icons.star,
-                        color: Colors.yellow[800],
+                    Row(
+                      children: [
+                        //star icon
+                        Icon(
+                          Icons.star,
+                          color: Colors.yellow[800],
+                        ),
+
+                        const SizedBox(
+                          width: 10,
+                        ),
+
+                        //rating number
+                        Text(
+                          widget.food.rating,
+                          style: TextStyle(
+                            color: Colors.grey[600],
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    const SizedBox(
+                      height: 10,
+                    ),
+
+                    //food name
+                    Text(
+                      widget.food.name,
+                      style: GoogleFonts.dmSerifDisplay(fontSize: 28),
+                    ),
+
+                    const SizedBox(
+                      height: 25,
+                    ),
+
+                    //description
+                    Text(
+                      "Description",
+                      style: TextStyle(
+                        color: Colors.grey[900],
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
                       ),
+                    ),
 
-                      const SizedBox(
-                        height: 10,
+                    const SizedBox(height: 10),
+
+                    const Text(
+                      "Delicate sliced, fresh salmon drapes elegantly over a pillow of perfectly seasoned shushi rice",
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 14,
+                        height: 2,
                       ),
-
-                      //rating number
-                    ],
-                  )
-
-                  //food name
-
-                  //description
-                ],
+                    )
+                  ],
+                ),
               ),
             ),
-          ],
-        )
 
-        // price + quantity + add to cart button,
-        );
+            // price + quantity + add to cart button
+            Container(
+              color: primaryColor,
+              child: Padding(
+                padding: const EdgeInsets.all(25),
+                child: Column(
+                  children: [
+                    //price
+                    Text(
+                      "\$" + widget.food.price,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+
+                    // quantity
+                    Row(
+                      children: [
+                        //minus button
+                        Container(
+                            child: IconButton(
+                          icon: Icon(Icons.remove),
+                          onPressed: decrementQuantity,
+                        ))
+
+                        //quantity
+
+                        //plus button
+                      ],
+                    )
+
+                    //add to cart
+                  ],
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
