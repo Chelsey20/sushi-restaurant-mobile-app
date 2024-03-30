c699aa6feae8e6181558854fda1c45c7bb103f6f
Chichi <chelseygranzoescanlar@gmail.com>
2024-03-30 21:06:16 +0800

    Added Pay Now button in Cart Page

--- sushi_restaurant_app/lib/pages/cart_page.dart
+++ sushi_restaurant_app/lib/pages/cart_page.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sushi_restaurant_app/themes/colors.dart';
+import '../components/button.dart';
 import '../models/food.dart';
 import '../models/shop.dart';
 
@@ -19,47 +21,61 @@ class CartPage extends StatelessWidget {
           elevation: 0,
           backgroundColor: primaryColor,
         ),
-        body: ListView.builder(
-          itemCount: value.cart.length,
-          itemBuilder: (context, index) {
-            //get food from cart
-            final Food food = value.cart[index];
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: value.cart.length,
+                itemBuilder: (context, index) {
+                  //get food from cart
+                  final Food food = value.cart[index];
 
-            //get food name
-            final String foodName = food.name;
+                  //get food name
+                  final String foodName = food.name;
 
-            //get food price
-            final String foodPrice = food.price;
+                  //get food price
+                  final String foodPrice = food.price;
 
-            //return list tile
-            return Container(
-              decoration: BoxDecoration(
-                color: secondaryColor,
-                borderRadius: BorderRadius.circular(8),
+                  //return list tile
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: secondaryColor,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    margin: const EdgeInsets.only(left: 20, top: 20, right: 20),
+                    child: ListTile(
+                      title: Text(
+                        foodName,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      subtitle: Text(
+                        foodPrice,
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                      trailing: IconButton(
+                        icon: const Icon(
+                          Icons.delete,
+                          color: Colors.white,
+                        ),
+                        onPressed: removeFromCart,
+                      ),
+                    ),
+                  );
+                },
               ),
-              margin: const EdgeInsets.only(left: 20, top: 20, right: 20),
-              child: ListTile(
-                title: Text(
-                  foodName,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                subtitle: Text(
-                  foodPrice,
-                  style: const TextStyle(color: Colors.white),
-                ),
-                trailing: IconButton(
-                  icon: const Icon(
-                    Icons.delete,
-                    color: Colors.white,
-                  ),
-                  onPressed: removeFromCart,
-                ),
+            ),
+            //pay button
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: MyButton(
+                text: "Pay Now",
+                onTap: () {},
               ),
-            );
-          },
+            ),
+          ],
         ),
       ),
     );
