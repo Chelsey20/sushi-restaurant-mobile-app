c447a8b20fa2f3f901b740ca697f87a66e2fe1b7
Chichi <chelseygranzoescanlar@gmail.com>
2024-03-30 14:59:24 +0800

    Added shop.dart, add and remove to cart methods

--- sushi_restaurant_app/lib/pages/main.dart
+++ sushi_restaurant_app/lib/pages/main.dart
@@ -1,9 +1,16 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sushi_restaurant_app/models/shop.dart';
 import 'package:sushi_restaurant_app/pages/intro_page.dart';
 import 'menu_page.dart';
 
 void main() {
-  runApp(const SushiRestaurantApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => Shop(),
+    child: const SushiRestaurantApp(),
+  ));
 }
 
 class SushiRestaurantApp extends StatelessWidget {
@@ -13,7 +20,7 @@ class SushiRestaurantApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Sushi Restaurant App',
-        home: IntroPage(),
+        home: const IntroPage(),
         routes: {
           '/intropage': (context) => const IntroPage(),
           '/menupage': (context) => const MenuPage(),
